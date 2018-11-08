a76c70472e04a030779d2cac4a559059cf730c53
Maxime Moison <moisonmaxime@gmail.com>
2018-11-08 12:46:28 -0800

    [MM] moved token to user table

--- database/migration/user.sql
+++ database/migration/user.sql
@@ -2,6 +2,7 @@ CREATE TABLE user (
     u_id INT PRIMARY KEY,
     u_name CHAR(25) not null,
     u_email VARCHAR(50) not null,
+    u_api_key VARCHAR(50) not null,
     u_username VARCHAR(25) not null,
     u_password VARCHAR(25) not null
 );
