6719776b12ea1516447198e06f00cdea0fbe7355
Rudra Aiyar <rudra.aiyar@gmail.com>
2018-11-08 16:10:45 -0800

    fixed all vs get

--- services/containers_service.js
+++ services/containers_service.js
@@ -17,7 +17,7 @@ exports.create_containers = function(req, res) {
     let ctype = req.params.ctype;
     let cid = req.params.cid;
 
-    db.all(`INSERT INTO container
+    db.get(`INSERT INTO container
     (c_id, c_name, c_type)
     VALUES ($cid ,$cname, $ctype)`, { $username: username }, { $cid: cid}, { $cname: cname}, {$ctype: ctype}, function(err, result) {
             res.send(result);
@@ -31,7 +31,7 @@ exports.update_containers = function(req, res) {
     let ctype = req.params.ctype;
     let cid = req. params.cid;
 
-    db.all(`UPDATE container 
+    db.get(`UPDATE container 
     SET c_name = $cname,
     c_type= $ctype
     FROM container, user, user_container
@@ -45,7 +45,7 @@ exports.delete_containers = function(req, res) {
     let username = req.params.username;
     let cname = req.params.cname;
 
-    db.all(`DELETE 
+    db.get(`DELETE 
     FROM container, user_container, user 
     where uc_user_id = u_id 
     and uc_c_id = c_id 
