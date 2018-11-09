5bd0821f7e053d151a681a050021fed3cbb58bab
Rudra Aiyar <rudra.aiyar@gmail.com>
2018-11-08 16:01:15 -0800

    containers updates, true love

--- endpoints/containers_endpoint.js
+++ endpoints/containers_endpoint.js
@@ -2,5 +2,14 @@ module.exports = function(app) {
     var service = require('../services/containers_service');
 
     app.route('/containers/:username')
-        .get(service.get_containers);
+        .get(service.get_containers)
+
+    app.route('/containers/:username/:cname/:ctype')
+        .post(service.create_containers)
+
+    app.route('/containers/:username/:cname')
+        .delete(service.delete_containers)
+
+    app.route('/containers/:username/:cname/:ctype/:cid')
+        .put(service.update_containers)
 };
