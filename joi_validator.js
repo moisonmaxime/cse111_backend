cf1dcb09e4aa21e9cf92bbfc89dd1ba9d3dbf29c
Maxime Moison <moisonmaxime@gmail.com>
2018-11-12 22:39:03 -0800

    [MM] added joi middleware

---
+++ joi_validator.js
@@ -0,0 +1,32 @@
+let Joi = require('joi');
+
+// --- Auth schemas ---
+const loginSchema = Joi.object({
+    body: Joi.object({
+        username: Joi.string().min(3).max(50).required(),
+        password: Joi.string().min(3).max(50).required()
+    })
+}).unknown();
+exports.loginSchema = loginSchema;
+
+const registerSchema = Joi.object({
+    body: Joi.object({
+        username: Joi.string().min(3).max(50).required(),
+        password: Joi.string().min(3).max(50).required(),
+        email: Joi.string().min(3).max(50).required(),
+        name: Joi.string().min(3).max(50).required(),
+        password: Joi.string().min(3).max(50).required()
+    })
+}).unknown();
+exports.registerSchema = registerSchema;
+
+
+// --- Joi Express Middleware ---
+function validate(schema) {
+    return function(req, res, next) {
+        let error = schema.validate(req).error;
+        if (error) return res.status(422).send('Invalid parameters');
+        next();
+    }
+}
+exports.validate = validate
