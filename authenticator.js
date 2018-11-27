454598ac53b2d5aa2883fda8420a5abe44465fe7
Maxime Moison <moisonmaxime@gmail.com>
2018-11-27 12:31:51 -0800

    [MM] token is now in Authorization header

--- authenticator.js
+++ authenticator.js
@@ -4,8 +4,8 @@ let db = require('./db_manager');
 function authenticate() {
     return async function(req, res, next) {
         try {
-            let token = req.headers.bearer;
-            let foundUser = await db.get('Select u_id as id from user where u_token = $token', { $token: token });
+            let token = req.headers.Authorization;
+            let foundUser = await db.get('Select u_id as id,  from user where u_token = $token', { $token: token });
             if (!foundUser) return res.status(403).send('Not Authorized');
             req.user = foundUser;
             next();
