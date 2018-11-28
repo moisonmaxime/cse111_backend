25e06c17b09cea1bf53ddb99057c6cf6c966fd05
Maxime Moison <moisonmaxime@gmail.com>
2018-11-27 22:36:13 -0800

    [MM] improved auth system  • JWT implementation  • aes256 RSA key  • bcrypt abstraction

---
+++ auth.js
@@ -0,0 +1,28 @@
+const bcrypt = require('bcrypt'),
+    jwt = require('jsonwebtoken'),
+    fs = require('fs');
+
+const secretKey = fs.readFileSync('./key.pem');
+const jwtExpirationTime = 86400*365; // 365 days in seconds
+
+function hashPassword(password) {
+    return bcrypt.hash(password, 8);
+};
+exports.hashPassword = hashPassword;
+
+function checkPassword(password, hash) {
+    return bcrypt.compare(password, hash);
+}
+exports.checkPassword = checkPassword;
+
+function signJWT(username) {
+    return jwt.sign({ username: username }, secretKey, {
+        expiresIn: jwtExpirationTime
+    });
+};
+exports.signJWT = signJWT;
+
+function verifyJWT(token) {
+    return jwt.verify(token, secretKey).username
+};
+exports.verifyJWT = verifyJWT;
