d1b1071bb89539939362325f171bf78d317a5c8a
Maxime Moison <moisonmaxime@gmail.com>
2018-11-27 22:37:32 -0800

    [MM] Tokens are not stored in DB anymore

--- database/migration/user.sql
+++ database/migration/user.sql
@@ -2,8 +2,7 @@ CREATE TABLE user (
     u_id INTEGER PRIMARY KEY AUTOINCREMENT,
     u_name CHAR(25) not null,
     u_email VARCHAR(50) not null,
-    u_token VARCHAR(255) not null,
-    u_username VARCHAR(25) not null,
+    u_username VARCHAR(25) UNIQUE not null,
     u_password VARCHAR(72) not null,
     u_type VARCHAR (25) not null
 );
